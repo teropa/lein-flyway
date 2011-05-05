83bc2283ca34df4a8a95a9922611d06cf96cf948
Manoj Waikar <mmwaikar@gmail.com>
2011-05-05 11:25:08 +0530

    Update the dependency on flyway from 0.9.3 to 1.3.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-flyway "1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.googlecode.flyway/flyway-core "0.9.3"]
+                 [com.googlecode.flyway/flyway-core "1.3.1"]
                  [commons-dbcp "1.4"]
                  [org.springframework/spring-jdbc "2.5.6"]]
   :eval-in-leiningen true)
