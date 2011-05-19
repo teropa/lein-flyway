b34589e1d68906e82c784131504dfc66130570d4
Manoj Waikar <mmwaikar@gmail.com>
2011-05-19 15:52:51 +0530

    Update the dependency on flyway from 1.3.1 to 1.4, add a :flyway section so that the correct argument is passed to the project parameter of various flyway methods.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject lein-flyway "1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.googlecode.flyway/flyway-core "1.3.1"]
+                 [com.googlecode.flyway/flyway-core "1.4"]
                  [commons-dbcp "1.4"]
                  [org.springframework/spring-jdbc "2.5.6"]
                  [postgresql "9.0-801.jdbc4"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :flyway {:driver "org.postgresql.Driver" :url "jdbc:postgresql:flyway" :username "postgres" :password ""})
 
